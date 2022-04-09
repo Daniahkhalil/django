5a1de3d7bf0d4a4661cb672a2e15db76f71dd237
Daniahkhalil <dhkhalil99@gmail.com>
2022-04-09 16:24:16 +0300

    Login registration update

--- Login_and_Registration2/login_Reg/urls.py
+++ Login_and_Registration2/login_Reg/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path     
 from . import views
+
 urlpatterns = [
     path('', views.index),
-    path('reg_log', views.regiser),  
-    path('login', views.login),
+    path('reg_log',views.register),
+    path('home',views.home_page),
+    path('login',views.login),
     path('logout',views.logout)
+
+    	   
 ]
--- Login_and_Registration2/login_Reg/views.py
+++ Login_and_Registration2/login_Reg/views.py
@@ -1,74 +1,70 @@
-from genericpath import exists
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.db import models
 from .models import *
-from . import models
 from django.contrib import messages
 import bcrypt
 
-# Create your views here.
 def index(request):
     return render(request,'index.html')
 
-def regiser(request):
-    errors = User.objects.basic_validator(request.POST)
-        # check if the errors dictionary has anything in it
-    if len(errors) > 0:
-        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
-        for key, value in errors.items():
-            messages.error(request, value)
-    
-        # redirect the user back to the form to fix the errors
-    else:
-        # if the errors object is empty, that means there were no errors!
-            # include some logic to validate user input before adding them to the database!
-        password = request.POST['passward']
-        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  # create the hash    
-        print(pw_hash)      # prints something like b'$2b$12$sqjyok5RQccl9S6eFLhEPuaRaJCcH3Esl2RWLm/cimMIEnhnLb7iC' 
-        User.objects.create(
-        first_name=request.POST['first_name'],
-        last_name=request.POST['last_name'],
-        email=request.POST['email'],
-        password=pw_hash,
-
-    )
+def home_page(request):
+    if 'logged_user_id' in request.session:
+        context={
+            'user_name':request.session['user_name'],
+            'user_id':request.session['logged_user_id']
+        }
+        return render(request,'home_page.html',context)
     return redirect('/')
+
+def register(request):
+    print('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+    if request.method == 'POST':
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            password = request.POST['passward']
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  
+            print(pw_hash)    
+            new_user=User.objects.create(
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email'],
+            password=pw_hash)
+            # if new_user is not None:
+            #     if not request.session:
+            request.session['logged_user_id']= new_user.id
+            print(new_user.id)
+            request.session['user_name']= new_user.first_name
+            print(new_user.first_name)
+            return redirect('/home')
 
 def login(request ):
 
-    errors=User.objects.log_validator(request.POST)
-            # check if the errors dictionary has anything in it
-    if len(errors) > 0:
-        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
-        for key, value in errors.items():
-            messages.error(request, value)
-        # redirect the user back to the form to fix the errors
-    else:
-        email=request.POST['email_log']
-        object_user=User.objects.get(email=email)
-        if not User.objects.filter(email=email).exists():
+    if request.method == 'POST':
+        errors=User.objects.log_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
             return redirect('/')
-            # return render(request,'success.html')
-           # see if the username provided exists in the database
-        user = User.objects.filter(email=request.POST['email_log']) # why are we using filter here instead of get?
-        if user: # note that we take advantage of truthiness here: an empty list will return false
-            logged_user = user[0] 
-        # assuming we only have one user with this username, the user would be first in the list we get back
-        # of course, we should have some logic to prevent duplicates of usernames when we create users
-        # use bcrypt's check_password_hash method, passing the hash from our database and the password from the form
-        if bcrypt.checkpw(request.POST['Passward_log'].encode(), logged_user.password.encode()):
-            # if we get True after checking the password, we may put the user id in session
-            request.session['userid'] = logged_user.id
-            # never render on a post, always redirect!
-            return render(request,'success.html')
-    # if we didn't find anything in the database by searching by username or if the passwords don't match, 
-    # redirect back to a safe route
+        else:
+            # email=request.POST['email_log']
+            # object_user=User.objects.get(email=email)
+            # if  User.objects.filter(email=email).exists():
+            # return redirect('/')
+            user = User.objects.filter(email=request.POST['email_log'])
+            # print(user.id)
+            if user: 
+                logged_user = user[0] 
+                if bcrypt.checkpw(request.POST['Passward_log'].encode(), logged_user.password.encode()):
+                    request.session['logged_user_id'] = logged_user.id
+                    request.session['user_name']=logged_user.first_name
+                    return redirect('/home')
+            return redirect('/')
     return redirect('/')
-
-    # return render(request,'success.html')
 
 def logout(request):
+    request.session.flush()
     return redirect('/')
-
-
-    
